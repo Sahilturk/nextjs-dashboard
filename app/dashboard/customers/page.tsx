ff82c1041249c8ab79458174536fb0ad35c6d809
sahil <sahilturk670@gmail.com>
2025-05-06 15:55:40 +0500

    customer route is created

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,14 +1,24 @@
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from '@/app/lib/data';
 export const metadata: Metadata = {
     title: "Customer",
     description: "Customer page",
-    icons: {
-        icon: "/favicon.ico",
-        shortcut: "/favicon.ico",
-        apple: "/favicon.ico",
-    },
 };
 
-export default function Page() {
-    return <p>Customer Page</p>;
+export default async function Page(props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+    return (
+        <main>
+        <CustomersTable customers={customers} />
+      </main>
+    );
 }  
